658ad1d0b3016d344ce737343d1ceb38dd05bf0d
Thiago Vasconcelos <122023431+thiagovasconcelosmiranda@users.noreply.github.com>
2024-12-06 21:38:17 -0300

    Atualização

--- src/controllers/book.ts
+++ src/controllers/book.ts
@@ -1,13 +1,20 @@
-import { Response, Request } from "express";
-import {prisma} from '../database';
+import { prisma } from '../database';
 
 export const getBooks = async (req: any, res: any) => {
     const books = await prisma.books.findMany();
-
     res.json(books);
 }
 
 
 export const addBooks = async (req: any, res: any) => {
-       res.json({name: req.body.name});
+    const { name, description } = req.body;
+    res.json(description)
+
+    const book = await prisma.books.create({
+        data: {
+            name: name,
+            description: description
+        }
+    });
+    res.json(book);
 }
